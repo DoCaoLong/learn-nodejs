b20e6cdcea800ddae93f6e8dac73445ef139fea9
DoCaoLong <bmt.long.faker@gmail.com>
2023-05-05 13:49:28 +0700

    sql

--- src/config/database.js
+++ src/config/database.js
@@ -18,7 +18,7 @@ const connection = mysql.createPool({
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10, // 10 thang connect
-  // maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  // maxIdle: 10 , // max idle connections, the default value is the same as `connectionLimit`
   // idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
   queueLimit: 0,
 });
